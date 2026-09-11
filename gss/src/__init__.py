7664488c62f3f9f6fe07f72abe66486f7ad5cf74
scarbozz2203 <sebozzcar@gmail.com>
2026-09-11 12:50:08 -0600

    GSS Delete

--- gss/src/__init__.py
+++
@@ -1,6 +0,0 @@
-# ./src/__init__.py
-"""
-Package marker for the [./src] directory.
-
-The file contains no runtime logic.
-"""
--- gss/src/ui/__init__.py
+++
@@ -1,6 +0,0 @@
-# ./src/ui/__init__.py
-"""
-Package marker for the [./src/ui] directory.
-
-The file contains no runtime logic.
-"""
